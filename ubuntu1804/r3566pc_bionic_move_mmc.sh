b77f48e0fb2d48910370b9678b58fdcd6f59208a
snakajim <satohi.nakajima@arm.com>
2022-04-29 11:42:04 +0000

    bug fix

--- ubuntu1804/r3566pc_bionic_move_mmc.sh
+++ ubuntu1804/r3566pc_bionic_move_mmc.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # r3566pc_bionic_move_mmc.sh
 #
-apt -y update && apt -y upgrade
+sudo apt -y update
 sudo dpkg-reconfigure keyboard-configuration
 
 #
 # mount MMC and copy
 #
 sudo fdisk /dev/mmcblk0
-sudo reboot
+#sudo reboot
 
 # Partion 1:  +1G -> /tmp
 # Partion 2: +10G -> /var
@@ -22,27 +22,57 @@ sudo mkfs.ext4 /dev/mmcblk0p4
 #
 # mount, copy and set /etc/fstab
 #
+sudo cp /etc/fstab /etc/fstab.back
+sudo cp /etc/fstab /etc/fstab.new
 
 # Partion 1:  +1G -> /tmp
 TARGET_DIR="/mnt/tmp"
 PARTISION_ID="p1"
 sudo mkdir -p ${TARGET_DIR} && sudo mount -t ext4 -o defaults /dev/mmcblk0${PARTISION_ID} ${TARGET_DIR}
 cd / && sudo sh -c "tar cf - ./tmp | ( cd ${TARGET_DIR}; tar xvf -)"
+MOUNT_DIR=`echo ${TARGET_DIR} | sed "s#^/mnt##"`
+cat << EOF | sudo tee -a /etc/fstab.new
+/dev/mmcblk0${PARTISION_ID}  ${MOUNT_DIR} ext3    defaults       1        2 
+EOF
+sudo umount ${TARGET_DIR}
 
 # Partion 2:  +10G -> /var
 TARGET_DIR="/mnt/var"
 PARTISION_ID="p2"
 sudo mkdir -p ${TARGET_DIR} && sudo mount -t ext4 -o defaults /dev/mmcblk0${PARTISION_ID} ${TARGET_DIR}
 cd / && sudo sh -c "tar cf - ./var | ( cd ${TARGET_DIR}; tar xvf -)"
+MOUNT_DIR=`echo ${TARGET_DIR} | sed "s#^/mnt##"`
+cat << EOF | sudo tee -a /etc/fstab.new
+/dev/mmcblk0${PARTISION_ID}  ${MOUNT_DIR} ext3    defaults       1        2 
+EOF
+sudo umount ${TARGET_DIR}
 
-# Partion 3:  +10G -> /var
-TARGET_DIR="/mnt/usr/local"
+# Partion 3:  +10G -> /usr
+TARGET_DIR="/mnt/usr"
 PARTISION_ID="p3"
 sudo mkdir -p ${TARGET_DIR} && sudo mount -t ext4 -o defaults /dev/mmcblk0${PARTISION_ID} ${TARGET_DIR}
-cd / && sudo sh -c "tar cf - ./usr/local | ( cd ${TARGET_DIR}; tar xvf -)"
+cd / && sudo sh -c "tar cf - ./usr | ( cd ${TARGET_DIR}; tar xvf -)"
+MOUNT_DIR=`echo ${TARGET_DIR} | sed "s#^/mnt##"`
+cat << EOF | sudo tee -a /etc/fstab.new
+/dev/mmcblk0${PARTISION_ID}  ${MOUNT_DIR} ext3    defaults       1        2 
+EOF
+sudo umount ${TARGET_DIR}
 
 # Partion 4:  37.3G -> /home
 TARGET_DIR="/mnt/home"
-PARTISION_ID="p3"
+PARTISION_ID="p4"
 sudo mkdir -p ${TARGET_DIR} && sudo mount -t ext4 -o defaults /dev/mmcblk0${PARTISION_ID} ${TARGET_DIR}
 cd / && sudo sh -c "tar cf - ./home | ( cd ${TARGET_DIR}; tar xvf -)"
+MOUNT_DIR=`echo ${TARGET_DIR} | sed "s#^/mnt##"`
+cat << EOF | sudo tee -a /etc/fstab.new
+/dev/mmcblk0${PARTISION_ID}  ${MOUNT_DIR} ext3    defaults       1        2 
+EOF
+sudo umount ${TARGET_DIR}
+
+#
+cat /etc/fstab.new
+read -p "Overwrite /etc/fstab,Ok?[Enter]"
+sudo /etc/fstab.new /etc/fstab
+mount -a
+read -p "reboot,Ok?[Enter]"
+sudo reboot
