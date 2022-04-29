9c7feab3313409774f80e01bc80bd592ef113842
snakajim <satohi.nakajima@arm.com>
2022-04-29 10:54:00 +0000

    update

--- ubuntu1804/r3566pc_bionic_move_mmc.sh
+++ ubuntu1804/r3566pc_bionic_move_mmc.sh
@@ -27,16 +27,22 @@ sudo mkfs.ext4 /dev/mmcblk0p4
 TARGET_DIR="/mnt/tmp"
 PARTISION_ID="p1"
 sudo mkdir -p ${TARGET_DIR} && sudo mount -t ext4 -o defaults /dev/mmcblk0${PARTISION_ID} ${TARGET_DIR}
-cd / && tar cf - ./tmp | ( cd ${TARGET_DIR}; tar xvf -)
+cd / && sudo sh -c "tar cf - ./tmp | ( cd ${TARGET_DIR}; tar xvf -)"
 
 # Partion 2:  +10G -> /var
 TARGET_DIR="/mnt/var"
 PARTISION_ID="p2"
 sudo mkdir -p ${TARGET_DIR} && sudo mount -t ext4 -o defaults /dev/mmcblk0${PARTISION_ID} ${TARGET_DIR}
-cd / && tar cf - ./var | ( cd ${TARGET_DIR}; tar xvf -)
+cd / && sudo sh -c "tar cf - ./var | ( cd ${TARGET_DIR}; tar xvf -)"
 
 # Partion 3:  +10G -> /var
 TARGET_DIR="/mnt/usr/local"
 PARTISION_ID="p3"
 sudo mkdir -p ${TARGET_DIR} && sudo mount -t ext4 -o defaults /dev/mmcblk0${PARTISION_ID} ${TARGET_DIR}
-cd / && tar cf - ./usr/local | ( cd ${TARGET_DIR}; tar xvf -)
+cd / && sudo sh -c "tar cf - ./usr/local | ( cd ${TARGET_DIR}; tar xvf -)"
+
+# Partion 4:  37.3G -> /home
+TARGET_DIR="/mnt/home"
+PARTISION_ID="p3"
+sudo mkdir -p ${TARGET_DIR} && sudo mount -t ext4 -o defaults /dev/mmcblk0${PARTISION_ID} ${TARGET_DIR}
+cd / && sudo sh -c "tar cf - ./home | ( cd ${TARGET_DIR}; tar xvf -)"
